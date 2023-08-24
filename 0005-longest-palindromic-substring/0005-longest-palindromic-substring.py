d0f0af49d15929a210b9155d44d75ef307dc7366
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2023-08-24 18:41:01 +0800

    Time: 3451 ms (24.92%), Space: 16.4 MB (44.47%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,8 +6,12 @@ class Solution:
             position = [j for j in range(len(s)) if s[i] == s[j]]
             for j in position:
                 tmp = s[i : j + 1]
+                if (len(tmp) <= len(palindrome)):
+                    continue
                 if (tmp == tmp[::-1]):
                     longest_string = tmp
-            if (longest_string is not None and len(longest_string) > len(palindrome)):
+            if (longest_string is None):
+                continue 
+            if (len(longest_string) > len(palindrome)):
                 palindrome = longest_string
         return palindrome
