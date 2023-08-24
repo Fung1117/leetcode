db3b38cc140ba86f01af0ac99b6ab57b360c861d
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2023-08-24 18:45:34 +0800

    Time: 1673 ms (38.14%), Space: 16.6 MB (24.98%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,20 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        palindrome = s[0]
-        for i in range(len(s)):
-            longest_string = str()
 
-            position = [j for j in range(len(s)) if s[i] == s[j]]
-            
-            
+        longestString = s[0]
+
+        for i in range(len(s)):
+            subString = ""
+            position = [j for j in range(i+1, len(s)) if s[j] == s[i]]
+            if (len(position) == 0 or position[-1] - i < len(longestString) ):
+                continue
             for j in position:
-                tmp = s[i : j + 1]
-                if (len(tmp) <= len(palindrome)):
-                    continue
-                if (tmp == tmp[::-1]):
-                    longest_string = tmp
-            if (longest_string is None):
-                continue 
-            if (len(longest_string) > len(palindrome)):
-                palindrome = longest_string
-        return palindrome
+                tempString = s[i:j + 1]
+                if (tempString != tempString[::-1]):
+                    tempString = ""
+                if (tempString != ""):
+                    subString = tempString
+            if (len(subString) > len(longestString)):
+                longestString = subString 
+        
+        return longestString
