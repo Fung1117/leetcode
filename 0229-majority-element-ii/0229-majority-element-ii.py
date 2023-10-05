f5dbdbad6a0bc8c8ccdcf83f58e55108af7c51ff
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2023-10-05 13:13:05 +0800

    Time: 118 ms (24.43%), Space: 17.6 MB (84.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,5 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        dir_nums = {}
+        ary_nums = []
         target = len(nums) // 3
-        set_nums = set(nums)
-        return [i for i in set_nums if (nums.count(i) > target)]
+        for i in nums:
+            if (i in ary_nums):
+                continue
+            if (i not in dir_nums):
+                dir_nums[i] = 1
+            else:
+                dir_nums[i] += 1
+            if (dir_nums[i] > target):
+                ary_nums.append(i)
+        return ary_nums
