5f4e39f24d106a42e9d6ddc4f92ae920748f806e
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2024-05-21 15:43:37 +0800

    Time: 35 ms (72.93%), Space: 16.7 MB (77.55%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,8 +2,9 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         power_set = [ [] ]
         for i in nums:
-            tmp = []
-            for j in power_set:
-                tmp.append(j + [i])
-            power_set = power_set + tmp
+            times = len(power_set)
+            count = 0
+            while count != times:
+                power_set.append(power_set[count] + [i])
+                count += 1
         return power_set
