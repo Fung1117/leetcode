29a4da1d3af892c725aa0862e56f3f80d9d2aea0
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2024-09-24 12:25:33 +0800

    Time: 680 ms (94.96%), Space: 28 MB (94.03%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,32 +1,18 @@
-class TrieNode:
-    def __init__(self):
-        self.children =  [None for _ in range(10)]
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-    def insert(self, value: int)->None:
-        value = str(value)
-        current = self.root
-        for val in value:
-            if current.children[int(val)] is None:
-                current.children[int(val)] = TrieNode()
-            current = current.children[int(val)]
-    def findLongestPrefix(self, value: int)-> int:
-        value = str(value)
-        current = self.root
-        count = 0
-        for val in value:
-            if current.children[int(val)] is None:
-                return count
-            current = current.children[int(val)]
-            count += 1
-        return count
-
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        # make use of trie
-        trie = Trie()
+        prefixArr1 = set()
         for value in arr1:
-            trie.insert(value)
-        return max( trie.findLongestPrefix(value) for value in arr2)
-
+            while value > 0:
+                if value in prefixArr1:
+                    break
+                prefixArr1.add(value)
+                value //= 10
+        maxCommonPrefix = 0
+        for value in arr2:
+            while value > 0 and len(str(value)) > maxCommonPrefix:
+                if value in prefixArr1:
+                    maxCommonPrefix = len(str(value))
+                    break
+                else:
+                    value //= 10
+        return maxCommonPrefix
