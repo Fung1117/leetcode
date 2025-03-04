6ea9070fcc907ef0fd4ac3e2d2ba448ea2ca2b75
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2025-03-04 12:29:50 +0800

    Time: 207 ms (7.97%), Space: 17.7 MB (88.25%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -2,15 +2,15 @@ import math
 
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-        max_bound = int(math.log(n, 3)) + 2
+
         def helper(num, i):
-            if num > n or i > max_bound:
-                return False
-            
-            if num == n:
+            if num == 0:
                 return True
+                
+            if 3 ** i > num:
+                return False
             else:
-                return helper(num, i + 1) or helper(num + 3**i , i + 1)
+                return helper( num, i + 1) or helper( num-3**i, i + 1)
         
-        return helper(0, 0)
+        return helper(n, 0)
         
