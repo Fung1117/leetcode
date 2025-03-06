78a97d108a46067c5b0f63336cd325c8e1129fbb
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2025-03-06 10:01:33 +0800

    Time: 11 ms (46.96%), Space: 18.2 MB (45%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -1,18 +1,18 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n = len(grid)
-        repeated = 0
         nums = set()
+        repeated = 0
         int_range = set(range(1, n**2 + 1))
-        print(int_range)
         
         for row in grid:
             for i in row:
                 if i in nums:
                     repeated = i
                 nums.add(i)
+        
         missed = list(int_range - nums)[0]
-        print(missed)
+
         return [repeated, missed]
                 
         
